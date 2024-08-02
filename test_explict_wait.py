54b5ad8002dcb7df19aedf15037a8b8be5028578
Ronfel <ronfelara@gmail.com>
2024-08-02 11:19:35 -0300

    Add Test Impli_Explic

---
+++ test_explict_wait.py
@@ -0,0 +1,30 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.implicitly_wait(12)
+
+browser.maximize_window()
+browser.get("https://www.hyrtutorials.com/p/waits-demo.html#google_vignette")
+
+wait = WebDriverWait(browser, 30)
+
+btn1 = browser.find_element(By.ID, "btn1")
+btn2 = browser.find_element(By.ID, "btn2")
+
+
+btn1.click()
+browser.exp(7)
+
+txt1 = browser.find_element(By.ID, "txt1")
+print(txt1.is_displayed())
+
+btn2.click()
+browser.implicitly_wait(14)
+
+txt2 = browser.find_element(By.ID, "txt2")
+print(txt2.is_displayed())
+
+browser.quit()
